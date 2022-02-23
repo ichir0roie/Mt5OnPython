37d6cccb772b9786344ab6c6ebd215a3bee850bc
ichir0roieHome <ichir0roie.com>
2022-02-23 15:21:39 +0900

    Account.py sample

---
+++ Dat/Account copy.py
@@ -0,0 +1,10 @@
+
+
+class Account:
+    def __init__(self, login, password, server):
+        self.login = login
+        self.password = password
+        self.server = server
+
+
+demo=Account(00000000,"","")
